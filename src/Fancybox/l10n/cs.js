88ff986fda1adab441dce0d4e9f99825431c37c9
Lukáš Janeček <luky.janecek@gmail.com>
2021-07-21 16:44:21 +0200

    Add Czech translation
    Add Czech translation

---
+++ src/Fancybox/l10n/cs.js
@@ -0,0 +1,12 @@
+export default {
+    CLOSE: "Zavřít",
+    NEXT: "Další",
+    PREV: "Předchozí",
+    MODAL: "Toto okno můžete zavřít stisknutím klávesy ESC",
+    ERROR: "Někde se něco pokazilo, prosím zkuste akci opakovat později",
+    IMAGE_ERROR: "Obrázek nenalezen",
+    ELEMENT_NOT_FOUND: "HTML element nenalezen",
+    AJAX_NOT_FOUND: "Chyba AJAX načítaní: Not Found",
+    AJAX_FORBIDDEN: "Chyba AJAX načítaní: Forbidden",
+    IFRAME_ERROR: "Stránku nelze načíst"
+};

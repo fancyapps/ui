4925c98377bd4ccd0b468a5558a54b1fc43c6648
jehadja <jehad.jaghoub2@gmail.com>
2021-08-06 19:14:08 +0300

    Create ar.js
    Add Arabic Lanaguage

---
+++ src/Fancybox/l10n/ar.js
@@ -0,0 +1,17 @@
+export default {
+  CLOSE: "إغلاق",
+  NEXT: "التالي",
+  PREV: "السابق",
+  MODAL: "بامكانك اغلاق هذة النافذة بالضغط على المحتوى او ضغط زر ESC",
+  ERROR: "حدث خطأ ما، يرجى المحاولة لاحقا",
+  IMAGE_ERROR: "الصورة غير موجودة",
+  ELEMENT_NOT_FOUND: "عنصر HTML غير موجود",
+  AJAX_NOT_FOUND: "خطأ في التحميل: الصفحة غير موجودة",
+  AJAX_FORBIDDEN: "خطأ في التحميل: الصفحة محظورة",
+  IFRAME_ERROR: "خطأ في تحميل الصفحة",
+  TOGGLE_ZOOM: "تبديل مستوى التقريب",
+  TOGGLE_THUMBS: "تبديل الصور المصغرة",
+  TOGGLE_SLIDESHOW: "تبديل مستعرض الصور",
+  TOGGLE_FULLSCREEN: "تبديل وضع ملئ الشاشة",
+  DOWNLOAD: "تنزيل الملف",
+};

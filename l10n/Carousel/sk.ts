3ef9ab7ff1e2cbca8f572e59c81aa23e45629436
Daniel Rosa <drosa@factcool.com>
2023-10-01 09:55:36 +0200

    added Slovak translation

---
+++ l10n/Carousel/sk.ts
@@ -0,0 +1,5 @@
+export const cs = {
+  NEXT: "Ďalší",
+  PREV: "Predchádzajúci",
+  GOTO: "Prejsť na %d. snímok",
+};
---
+++ l10n/Fancybox/sk.ts
@@ -0,0 +1,20 @@
+import { cs as PANZOOM_CS } from "../Panzoom/cs";
+
+export const cs = {
+  ...PANZOOM_CS,
+  CLOSE: "Zavrieť",
+  NEXT: "Ďalší",
+  PREV: "Predchádzajúci",
+  MODAL: "Môžete zavrieť toto okno klávesou ESC",
+  ERROR: "Nastala chyba. Skúste to, prosím, znovu",
+  IMAGE_ERROR: "Obrázok sa nenašiel",
+  ELEMENT_NOT_FOUND: "HTML element sa nenašiel",
+  AJAX_NOT_FOUND: "Chyba načítania: nenájdené",
+  AJAX_FORBIDDEN: "Chyba načítania: zamietnuté",
+  IFRAME_ERROR: "Chyba načítania stránky",
+  TOGGLE_ZOOM: "Prepnúť úroveň priblíženia",
+  TOGGLE_THUMBS: "Zobraziť/skryť miniatúry",
+  TOGGLE_SLIDESHOW: "Spustiť/zastaviť automatické prehrávanie",
+  TOGGLE_FULLSCREEN: "Režim celej obrazovky",
+  DOWNLOAD: "Stiahnuť",
+};

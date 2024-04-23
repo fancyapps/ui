1318452fc6bf4c57377f739c53100d74ce89e458
Сергей <koreshs@mail.ru>
2024-04-23 15:08:28 +0300

    Create ru.ts

---
+++ l10n/Fancybox/ru.ts
@@ -0,0 +1,20 @@
+import { en as PANZOOM_EN } from "../Panzoom/en";
+
+export const ru = {
+  ...PANZOOM_EN,
+  CLOSE: "Закрыть",
+  NEXT: "Следущее",
+  PREV: "Предыдущее",
+  MODAL: "Можно закрыть это модальное окно нажав клавишу ESC.",
+  ERROR: "Что-то пошло не так. Пожалуйста, повторите позже",
+  IMAGE_ERROR: "Картинка не найдена",
+  ELEMENT_NOT_FOUND: "HTML элемент не найден",
+  AJAX_NOT_FOUND: "Ошибка загрузки AJAX: Не найдено",
+  AJAX_FORBIDDEN: "Ошибка загрузки AJAX: Запрещено",
+  IFRAME_ERROR: "Ошибка загрузки страницы",
+  TOGGLE_ZOOM: "Переключить уровень увеличения",
+  TOGGLE_THUMBS: "Переключить миниатюры",
+  TOGGLE_SLIDESHOW: "Переключить слайд-шоу",
+  TOGGLE_FULLSCREEN: "Переключить режим полного экрана",
+  DOWNLOAD: "Загрузка",
+};

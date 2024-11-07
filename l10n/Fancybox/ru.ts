568c45df03d5ffead2146fb702a065e12abae70a
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2024-11-07 21:42:28 +0500

    russian localization

---
+++ l10n/Fancybox/ru.ts
@@ -0,0 +1,20 @@
+import { ru as PANZOOM_RU } from "../Panzoom/ru";
+
+export const ru = {
+  ...PANZOOM_RU,
+  CLOSE: "Закрыть",
+  NEXT: "След.",
+  PREV: "Пред.",
+  MODAL: "Вы можете закрыть модальное оконо при помощи ESC",
+  ERROR: "Что-то пошло не так, пожалуйста, повторите еще раз",
+  IMAGE_ERROR: "Изображение не найдено",
+  ELEMENT_NOT_FOUND: "HTML элемент не найден",
+  AJAX_NOT_FOUND: "Ошибка загрузки AJAX : Не найдено",
+  AJAX_FORBIDDEN: "Ошибка загрузки AJAX : Запрещено",
+  IFRAME_ERROR: "Ошибка загрузки страницы",
+  TOGGLE_ZOOM: "Показать/скрыть зум",
+  TOGGLE_THUMBS: "Показать/скрыть миниатюры",
+  TOGGLE_SLIDESHOW: "Показать/скрыть слайд-шоу",
+  TOGGLE_FULLSCREEN: "Показать/скрыть полноэкранный режим",
+  DOWNLOAD: "Скачать",
+};
